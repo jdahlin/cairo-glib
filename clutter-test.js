6c275b529b18b87b48f1a174f6d53d57454439ae
Johan Dahlin <johan@gnome.org>
2009-09-23 11:53:16 -0300

    Add a clutter cairo test

---
+++ clutter-test.js
@@ -0,0 +1,19 @@
+const Clutter = imports.gi.Clutter;
+
+Clutter.init(0, null);
+
+let stage = Clutter.Stage.get_default();
+
+let texture = new Clutter.CairoTexture({surface_width: 500, surface_height: 500});
+stage.add_actor(texture);
+
+let cr = texture.create();
+cr.set_line_width(10.0);
+cr.set_source_rgba(1, 0.2, 0.2, 0.6);
+cr.arc(128, 128, 100, 45*(Math.PI/180.0), 180*(Math.PI/180.0));
+cr.fill();
+cr.destroy();
+
+stage.show_all();
+
+Clutter.main();
